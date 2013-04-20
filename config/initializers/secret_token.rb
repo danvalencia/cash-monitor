02332c3dca03af290b4c86214c8fc6e711e8c091
Omar <omar@obl-MintBookPro.(none)>
2013-04-20 15:26:57 -0500

    Agregando proyecto de rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cashmonitor2::Application.config.secret_token = 'd51ec4c185fb8fac7348ad0c332143be88567387a8b34511824ebaf35a3fb83825dddf5fdd82b57ac816b7d8e15b455d023246178abf9f15f08fb85f59a1b2c6'
