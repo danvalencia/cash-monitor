7a46b67eaa0bab0a4c32fe799149054d22761983
Daniel Valencia <danvalencia@gmail.com>
2013-10-14 18:32:25 -0700

    Adding user for Juanin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create!({email: 'danvalencia@gmail.com', password: 'password', password_confirmation: 'password', first_name: 'Daniel', last_name: 'Valencia', phone_number: '415-283-9462', admin: true})
+User.create!({email: 'juanmarcosvalencia@gmail.com', password: 'password', password_confirmation: 'password', first_name: 'Juan Marcos', last_name: 'Valencia', phone_number: '+52 1 (55) 4566 9412', admin: true})
