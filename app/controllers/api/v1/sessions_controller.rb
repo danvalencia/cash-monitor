1aebc983bc8948d85099d84526a61e87bfcf14be
Daniel Valencia <danvalencia@gmail.com>
2013-11-09 19:33:30 -0800

    Adding functionality to api: - When session exists, count is incremented. - When machine uuid does not exist, returns 404. - When db error happens, returns 500.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,8 +4,15 @@ class Api::V1::SessionsController < ApplicationController
 		session_uuid = params[:session_uuid]
 		machine_uuid = params[:machine_uuid]
 		machine = Machine.where(machine_uuid: machine_uuid).first
-		unless machine.nil?
+		if machine.nil?
+			respond_to do |format|
+				format.json {
+					render json: {message: 'Machine does not exist'}, status: 404 
+				}	 
+			end
+		else
 			@machine_session = Session.where(session_uuid: session_uuid).first
+			puts "Found machine session: #{@machine_session}"
 			if @machine_session.nil?
 				@machine_session = Session.new	do |s|
 					s.session_uuid = session_uuid
@@ -13,13 +20,27 @@ class Api::V1::SessionsController < ApplicationController
 					s.coin_count = 1
 				end
 				respond_to do |format|
-					if @machine_session.save!
+					if @machine_session.save
 						format.json {
 							render json: {message: 'Session Created'}, status: 201
 						} 
 					end
 				end
+			else
+				@machine_session.coin_count += 1
+				respond_to do |format|
+					if @machine_session.save
+						format.json {
+							render json: {message: 'Session Updated'}, status: 200
+						}
+					else
+						format.json {
+							render json: {message: 'Oops! seems like an internal error occured'}, status: 500
+						}
+					end
+				end
 			end
+
 		end
 	end
 end
--- test/functional/api/v1/sessions_controller_test.rb
+++ test/functional/api/v1/sessions_controller_test.rb
@@ -1,19 +1,64 @@
 require 'test_helper'
+require 'mocha/setup'
 
 class Api::V1::SessionsControllerTest < ActionController::TestCase
 
   def setup
     @valid_machine = machines(:one)
     @new_session_uuid = SecureRandom.uuid
+
+    @second_machine = machines(:two)
+    @existing_session = sessions(:one)
   end
 
   test "should return 201 with when machine id exists and sessions is new" do
     post :update, machine_uuid: @valid_machine.machine_uuid, session_uuid: @new_session_uuid, format: :json
-    puts "Response code: #{@response.code}"
     assert_response :success
     assert_equal 201, @response.code.to_i
     json_response = JSON.parse @response.body
     assert_not_nil json_response
   end
+
+  test "should return 200 when session exists" do
+    post :update, machine_uuid: @second_machine.machine_uuid, session_uuid: @existing_session.session_uuid, format: :json
+    assert_response :success
+    assert_equal 200, @response.code.to_i
+    json_response = JSON.parse @response.body
+    assert_not_nil json_response
+  end
+
+  test "should return 404 when machine uuid does not exist" do
+    post :update, machine_uuid: "fakeuuid", session_uuid: @existing_session.session_uuid, format: :json
+    assert_response :missing
+    assert_equal 404, @response.code.to_i
+    json_response = JSON.parse @response.body
+    assert_not_nil json_response
+  end
+
+  test "should return 500 when internal error occurs" do
+
+    my_machine = Machine.new do |m|
+      m.machine_uuid = "8ddca8b3-6bf8-4cbc-8965-6255b0169cdb"
+    end
+    my_session = Session.new do |s|
+      s.session_uuid = "13d6058b-feb8-4767-a85c-bc616d1835ca"
+      s.coin_count = 1
+    end
+
+    machines_result_set = [ my_machine ] 
+    Machine.expects(:where).with(machine_uuid: my_machine.machine_uuid).returns(machines_result_set)
+
+    sessions_result_set = [ my_session ]
+    Session.expects(:where).with(session_uuid: my_session.session_uuid).returns(sessions_result_set)
+    my_session.expects(:save).returns(false)
+
+    post :update, machine_uuid: my_machine.machine_uuid, session_uuid: my_session.session_uuid, format: :json
+    assert_response :error
+    assert_equal 500, @response.code.to_i
+    json_response = JSON.parse @response.body
+    assert_not_nil json_response
+  end
+
+  private
 
 end
