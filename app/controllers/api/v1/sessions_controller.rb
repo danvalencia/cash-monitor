83c7f211b42007e64c21d0c1cb29ddab153b0b4f
Daniel Valencia <danvalencia@gmail.com>
2013-11-16 11:56:42 -0800

    Refactoring for better readability

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -7,11 +7,8 @@ class Api::V1::SessionsController < ApplicationController
 		puts "Session: #{session_uuid}, Machine: #{machine_uuid}, Coin Count: #{coin_count}"
 		machine = Machine.where(machine_uuid: machine_uuid).first
 		if machine.nil?
-			respond_to do |format|
-				format.json {
-					render json: {message: 'Machine does not exist'}, status: 404 
-				}	 
-			end
+			response_message = {message: 'Machine does not exist'}
+			response_status = 404 
 		else
 			@machine_session = Session.where(session_uuid: session_uuid).first
 			if @machine_session.nil?
@@ -21,33 +18,26 @@ class Api::V1::SessionsController < ApplicationController
 					s.machine = machine
 					s.coin_count = coin_count
 				end
-				respond_to do |format|
-					if @machine_session.save
-						format.json {
-							render json: {message: 'Session Created'}, status: 201
-						} 
-					end
+				if @machine_session.save
+					response_message = {message: 'Session Created'}
+					response_status = 201 
 				end
 			else
 				if coin_count.nil?
-					render json: {message: 'Request should include the coin count'}, status: 400
+					response_message = {message: 'Request should include the coin count'}
+					response_status = 400 
 				else
 					@machine_session.coin_count = coin_count
-					respond_to do |format|
-						if @machine_session.save
-							puts "Machine Session Coin Count: #{@machine_session.coin_count}"
-							format.json {
-								render json: {message: 'Session Updated'}, status: 200
-							}
-						else
-							format.json {
-								render json: {message: 'Oops! seems like an internal error occured'}, status: 500
-							}
-						end
+					if @machine_session.save
+						response_message = {message: 'Session Updated'}
+						response_status = 200 
+					else
+						response_message = {message: 'Oops! seems like an internal error occured'}
+						response_status = 500 
 					end
 				end
 			end
-
 		end
+		render json: response_message, status: response_status
 	end
 end
