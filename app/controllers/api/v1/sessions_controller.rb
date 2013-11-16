cf08f0de0e4108d531f4907a2212a15f4e53c3d7
Daniel Valencia <danvalencia@gmail.com>
2013-11-16 11:47:59 -0800

    API now expects coin_count as part of the requests

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::SessionsController < ApplicationController
 	def update
 		session_uuid = params[:session_uuid]
 		machine_uuid = params[:machine_uuid]
+		coin_count = params[:coin_count]
+		puts "Session: #{session_uuid}, Machine: #{machine_uuid}, Coin Count: #{coin_count}"
 		machine = Machine.where(machine_uuid: machine_uuid).first
 		if machine.nil?
 			respond_to do |format|
@@ -12,12 +14,12 @@ class Api::V1::SessionsController < ApplicationController
 			end
 		else
 			@machine_session = Session.where(session_uuid: session_uuid).first
-			puts "Found machine session: #{@machine_session}"
 			if @machine_session.nil?
+				coin_count = 1 if coin_count.nil?
 				@machine_session = Session.new	do |s|
 					s.session_uuid = session_uuid
 					s.machine = machine
-					s.coin_count = 1
+					s.coin_count = coin_count
 				end
 				respond_to do |format|
 					if @machine_session.save
@@ -27,16 +29,21 @@ class Api::V1::SessionsController < ApplicationController
 					end
 				end
 			else
-				@machine_session.coin_count += 1
-				respond_to do |format|
-					if @machine_session.save
-						format.json {
-							render json: {message: 'Session Updated'}, status: 200
-						}
-					else
-						format.json {
-							render json: {message: 'Oops! seems like an internal error occured'}, status: 500
-						}
+				if coin_count.nil?
+					render json: {message: 'Request should include the coin count'}, status: 400
+				else
+					@machine_session.coin_count = coin_count
+					respond_to do |format|
+						if @machine_session.save
+							puts "Machine Session Coin Count: #{@machine_session.coin_count}"
+							format.json {
+								render json: {message: 'Session Updated'}, status: 200
+							}
+						else
+							format.json {
+								render json: {message: 'Oops! seems like an internal error occured'}, status: 500
+							}
+						end
 					end
 				end
 			end
--- test/functional/api/v1/sessions_controller_test.rb
+++ test/functional/api/v1/sessions_controller_test.rb
@@ -17,14 +17,33 @@ class Api::V1::SessionsControllerTest < ActionController::TestCase
     assert_equal 201, @response.code.to_i
     json_response = JSON.parse @response.body
     assert_not_nil json_response
+    assert_equal 1, Session.where(session_uuid: @new_session_uuid).first.coin_count
   end
 
-  test "should return 200 when session exists" do
-    post :update, machine_uuid: @second_machine.machine_uuid, session_uuid: @existing_session.session_uuid, format: :json
+  test "should return 200 when session exists and should update coin_count according to post parameter" do
+    # Initially, existing session has a coin count of 1
+    assert_equal 1, @existing_session.coin_count
+    post :update, machine_uuid: @second_machine.machine_uuid, session_uuid: @existing_session.session_uuid, coin_count: 3, format: :json
     assert_response :success
     assert_equal 200, @response.code.to_i
     json_response = JSON.parse @response.body
     assert_not_nil json_response
+
+    assert_equal 3, Session.where(session_uuid: @existing_session.session_uuid).first.coin_count
+  end
+
+  test "should return 400 when coin_count is not present in request and session exists" do
+    # Initially, existing session has a coin count of 1
+    assert_equal 1, @existing_session.coin_count
+
+    post :update, machine_uuid: @second_machine.machine_uuid, session_uuid: @existing_session.session_uuid, format: :json
+    assert_equal 400, @response.code.to_i
+    json_response = JSON.parse @response.body
+    assert_not_nil json_response
+
+    # Session coin count was not modified
+    assert_equal 1, Session.where(session_uuid: @existing_session.session_uuid).first.coin_count
+
   end
 
   test "should return 404 when machine uuid does not exist" do
@@ -52,7 +71,7 @@ class Api::V1::SessionsControllerTest < ActionController::TestCase
     Session.expects(:where).with(session_uuid: my_session.session_uuid).returns(sessions_result_set)
     my_session.expects(:save).returns(false)
 
-    post :update, machine_uuid: my_machine.machine_uuid, session_uuid: my_session.session_uuid, format: :json
+    post :update, machine_uuid: my_machine.machine_uuid, session_uuid: my_session.session_uuid, coin_count: 3, format: :json
     assert_response :error
     assert_equal 500, @response.code.to_i
     json_response = JSON.parse @response.body
