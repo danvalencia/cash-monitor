9fae25ff05ab3a4650d21e296b2878824f684d26
Daniel Valencia <danvalencia@gmail.com>
2013-11-17 14:49:31 -0800

    Adding functionality for closing session

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,10 +3,10 @@ class Api::V1::SessionsController < ApplicationController
 	def create
 		session_uuid = params[:session_uuid]
 		machine_uuid = params[:machine_uuid]
-		session_created_at = params[:session_created_at]
+		start_time = params[:start_time]
 
-		if session_created_at.nil?
-			response_message = {message: "session_created_at is a required parameter"}
+		if start_time.nil?
+			response_message = {message: "start_time is a required parameter"}
 			response_status = 400
 		else
 			machine = Machine.where(machine_uuid: machine_uuid).first
@@ -19,7 +19,7 @@ class Api::V1::SessionsController < ApplicationController
 					@machine_session = Session.new	do |s|
 						s.session_uuid = session_uuid
 						s.machine = machine
-						s.session_created_at = DateTime.parse session_created_at
+						s.start_time = DateTime.parse start_time
 						s.coin_count = 1
 					end
 					if @machine_session.save
@@ -43,6 +43,7 @@ class Api::V1::SessionsController < ApplicationController
 		session_uuid = params[:session_uuid]
 		machine_uuid = params[:machine_uuid]
 		coin_count = params[:coin_count]
+		end_time = params[:end_time]
 		machine = Machine.where(machine_uuid: machine_uuid).first
 		if machine.nil?
 			response_message = {message: 'Machine does not exist'}
@@ -53,22 +54,43 @@ class Api::V1::SessionsController < ApplicationController
 				response_message = {message: "Can't update session #{session_uuid} because it does not exist."}
 				response_status = 400 
 			else
-				if coin_count.nil?
+				if not end_time.nil? 
+					if @machine_session.end_time.nil?
+						end_time_obj = DateTime.parse end_time
+						if end_time_obj > @machine_session.start_time
+							@machine_session.end_time = end_time_obj
+							response_message, response_status = session_update
+						else
+							response_message = {message: 'End date is before start date'}
+							response_status = 400 
+						end
+					else
+						response_message = {message: 'Session is already closed'}
+						response_status = 400 
+					end
+				elsif coin_count.nil?
 					response_message = {message: 'Request should include the coin count'}
 					response_status = 400 
 				else
 					@machine_session.coin_count = coin_count
-					if @machine_session.save
-						response_message = {message: 'Session Updated'}
-						response_status = 200 
-					else
-						response_message = {message: 'Oops! seems like an internal error occured'}
-						response_status = 500 
-					end
+					response_message, response_status = session_update
 				end
 			end
 		end
 		render json: response_message, status: response_status
+	end
+
+	private
+
+	def session_update
+		if @machine_session.save
+			response_message = {message: 'Session Updated Successfully'}
+			response_status = 200 
+		else
+			response_message = {message: 'Oops! seems like an internal error occured'}
+			response_status = 500 
+		end
+		return response_message, response_status
 	end
 
 end
--- test/functional/api/v1/sessions_controller_test.rb
+++ test/functional/api/v1/sessions_controller_test.rb
@@ -9,19 +9,20 @@ class Api::V1::SessionsControllerTest < ActionController::TestCase
 
     @second_machine = machines(:two)
     @existing_session = sessions(:one)
+    @closed_session = sessions(:two)
   end
 
   test "should return 201 with when machine id exists and sessions is new" do
-    post :create, machine_uuid: @valid_machine.machine_uuid, session_uuid: @new_session_uuid, session_created_at: "2013-11-13 15:02:45", format: :json
+    post :create, machine_uuid: @valid_machine.machine_uuid, session_uuid: @new_session_uuid, start_time: "2013-11-13 15:02:45", format: :json
     assert_response :success
     assert_equal 201, @response.code.to_i
     json_response = JSON.parse @response.body
     assert_not_nil json_response
     new_session = Session.where(session_uuid: @new_session_uuid).first
     assert_equal 1, new_session.coin_count
-    assert_equal 11, new_session.session_created_at.month
-    assert_equal 13, new_session.session_created_at.day
-    assert_equal 2013, new_session.session_created_at.year
+    assert_equal 11, new_session.start_time.month
+    assert_equal 13, new_session.start_time.day
+    assert_equal 2013, new_session.start_time.year
   end
 
   test "should return 400 when doing POST (create method) on an existing session ID" do
@@ -55,7 +56,7 @@ class Api::V1::SessionsControllerTest < ActionController::TestCase
     Session.expects(:new).returns(my_session)
     my_session.expects(:save).returns(false)
 
-    post :create, machine_uuid: my_machine.machine_uuid, session_uuid: my_session.session_uuid, session_created_at: "2013-11-13 15:02:45", format: :json
+    post :create, machine_uuid: my_machine.machine_uuid, session_uuid: my_session.session_uuid, start_time: "2013-11-13 15:02:45", format: :json
     assert_response :error
     assert_equal 500, @response.code.to_i
     json_response = JSON.parse @response.body
@@ -74,15 +75,35 @@ class Api::V1::SessionsControllerTest < ActionController::TestCase
     assert_equal 3, Session.where(session_uuid: @existing_session.session_uuid).first.coin_count
   end
 
-  # test "should close session if close_session parameter is in the request" do
-  #   put :update, machine_uuid: @second_machine.machine_uuid, session_uuid: @existing_session.session_uuid, close_session: true, format: :json
-  #   assert_response :success
-  #   assert_equal 200, @response.code.to_i
-  #   json_response = JSON.parse @response.body
-  #   assert_not_nil json_response
+  test "should close session if end_time parameter is in the request" do
+    put :update, machine_uuid: @second_machine.machine_uuid, session_uuid: @existing_session.session_uuid, end_time: "2013-11-13 16:02:45", format: :json
+    assert_response :success
+    assert_equal 200, @response.code.to_i
+    json_response = JSON.parse @response.body
+    assert_not_nil json_response
+    end_time = Session.where(session_uuid: @existing_session.session_uuid).first.end_time
+    assert_not_nil end_time 
+    assert_equal 13, end_time .day
+    assert_equal 11, end_time .month
+    assert_equal 2013, end_time.year
+  end
 
-  #   assert_not_nil Session.where(session_uuid: @existing_session.session_uuid).first.closed_at
-  # end
+  test "should fail with 400 if end_time parameter is cronologically before start_time" do
+    before_start_time = @existing_session.start_time.yesterday
+    put :update, machine_uuid: @second_machine.machine_uuid, session_uuid: @existing_session.session_uuid, end_time: before_start_time.to_formatted_s(:db), format: :json
+    assert_equal 400, @response.code.to_i
+    json_response = JSON.parse @response.body
+    assert_not_nil json_response
+    end_time = Session.where(session_uuid: @existing_session.session_uuid).first.end_time
+    assert_nil end_time 
+  end
+
+  test "should fail with 400 when trying to close session twice" do
+    put :update, machine_uuid: @second_machine.machine_uuid, session_uuid: @closed_session.session_uuid, end_time: "2013-11-13 16:02:45", format: :json
+    assert_equal 400, @response.code.to_i
+    json_response = JSON.parse @response.body
+    assert_not_nil json_response
+  end  
 
   test "should return 400 when session does not exist on update" do
     # Initially, existing session has a coin count of 1
