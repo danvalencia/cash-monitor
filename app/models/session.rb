97a2569523d97fa8f1dbd460debb02d278e70868
Daniel Valencia <danvalencia@gmail.com>
2013-11-17 16:07:01 -0800

    Adding earnings calculation to session

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,16 @@
 class Session < ActiveRecord::Base
   attr_accessible :call_count, :call_time, :call_value, :coin_count, :coin_time, :coin_value, :end_time, :machine_id, :print_count, :print_time, :start_time
   belongs_to :machine
+
+  validates :machine_id, presence: true, allow_blank: false
+
+  before_save :assign_coin_value
+
+  def earnings
+ 		coin_count * coin_value
+  end
+
+  def assign_coin_value
+  	self.coin_value = machine.coin_value
+  end
 end
--- test/unit/session_test.rb
+++ test/unit/session_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class SessionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should give you total earnings" do
+  	mi_maquinet = machines(:one)
+
+		machine_session = Session.new	do |s|
+			s.session_uuid = "017d9c84-4f60-4113-82cf-febdaaadd789"
+			s.machine = mi_maquinet
+			s.start_time = DateTime.parse "2013-11-13 15:02:45" 
+			s.coin_count = 1
+		end
+		machine_session.save!
+
+		assert_equal 5, mi_maquinet.coin_value 
+		machine_session.coin_count += 1
+
+  	assert_equal 10, machine_session.earnings
+  end
 end
